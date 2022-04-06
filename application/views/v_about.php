9a07fdc1f403d0b43c73dac99e6c3dcfb2d55a4e
Fadli <nurahmadfadli01@gmail.com>
2022-04-07 04:39:59 +0700

    tugas pertemuan 3
    tugas pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+<h1><?php echo $judul ?></h1>
+<h4>Nama</h4>
+<ul type="disc">
+<li>Nama Depan : Nur Ahmad</li>
+<li>Nama Belakang : Fadli</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+<li> Kampung Baru</li>
+</ul>
+<h4>Tempat Lahir</h4>
+<ul type="none">
+<li>Jakarta</li>
+</ul>
+<h4>Olah Raga Favorit</h4>
+<ul type="square">
+<li>Bulutangkis</li>
+<li>Sepak Bola</li>
+</ul>
+</section>
